99786fbff1469cfcde457476336ae88cbd3501eb
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-05-28 15:44:45 +0700

    update ThreePhaseMonitor: add all data rst

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,6 @@
 import { PanelPlugin } from '@grafana/data';
 import { SimpleOptions } from './types';
-import { BatteryMonitor } from './BatteryMonitor';
+import { ThreePhaseMonitor } from './ThreePhaseMonitor';
 
-export const plugin = new PanelPlugin<SimpleOptions>(BatteryMonitor)
+export const plugin = new PanelPlugin<SimpleOptions>(ThreePhaseMonitor)
   .useFieldConfig();

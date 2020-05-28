08530a2fab833544146376f7dabac24fe15a4f6c
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-05-28 13:08:36 +0700

    update BatteryMonitor: remove field

--- src/BatteryMonitor.tsx
+++ src/BatteryMonitor.tsx
@@ -36,28 +36,12 @@ export const BatteryMonitor: React.FC<PanelProps> = (props) => {
     }
   }
 
-  const field: Partial<Field> = {
-    type: FieldType.number,
-    config: {
-      min: 0,
-      max: 100,
-      thresholds: {
-        mode: ThresholdsMode.Absolute,
-        steps: [
-          { value: -Infinity, color: 'red' },
-          { value: 50, color: 'green' },
-        ],
-      },
-    },
-  };
-  field.display = getDisplayProcessor({ field });
-
   return (
     <div className="w3-display-container" style={{ width: '100%', height: '100%' }}>
       <div className="w3-display-middle" style={{ width: '100%' }}>
         <HorizontalGroup justify="center">
           <div className="w3-margin-right">
-            <span className="w3-xxxlarge">{getLastValue(dataA)?.text || 'No Data'}</span><span className="w3-xxlarge">{getLastValue(dataA)?.suffix}</span>
+            <span className="w3-xxxlarge">{getLastValue(dataA)?.text}</span><span className="w3-xxlarge">{getLastValue(dataA)?.suffix}</span>
           </div>
           <BarGauge
             orientation={VizOrientation.Horizontal}
@@ -70,19 +54,19 @@ export const BatteryMonitor: React.FC<PanelProps> = (props) => {
               title: '',
               numeric: dataA?.fields[1].values.get(0)
             }}
-            field={field.config!}
-            display={field.display!}
+            field={dataA?.fields[1].config}
+            display={dataA?.fields[1].display}
           ></BarGauge>
         </HorizontalGroup>
         <div className="w3-section w3-row">
           <div className="w3-third w3-center">
-            <span className="w3-xlarge">{getLastValue(dataB)?.text || 'No Data'}</span><span className="w3-large">{getLastValue(dataB)?.suffix}</span>
+            <span className="w3-xlarge">{getLastValue(dataB)?.text}</span><span className="w3-large">{getLastValue(dataB)?.suffix}</span>
           </div>
           <div className="w3-third w3-center">
-            <span className="w3-xlarge">{getLastValue(dataC)?.text || 'No Data'}</span><span className="w3-large">{getLastValue(dataC)?.suffix}</span>
+            <span className="w3-xlarge">{getLastValue(dataC)?.text}</span><span className="w3-large">{getLastValue(dataC)?.suffix}</span>
           </div>
           <div className="w3-third w3-center">
-            <span className="w3-xlarge">{getLastValue(dataD)?.text || 'No Data'}</span><span className="w3-large">{getLastValue(dataD)?.suffix}</span>
+            <span className="w3-xlarge">{getLastValue(dataD)?.text}</span><span className="w3-large">{getLastValue(dataD)?.suffix}</span>
           </div>
         </div>
         <div></div>

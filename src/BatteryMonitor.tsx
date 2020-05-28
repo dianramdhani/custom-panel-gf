c84218fd2a125df337f50f5721c0deb38e06d7c6
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-05-28 12:20:51 +0700

    update BatteryMonitor: add unit

--- src/BatteryMonitor.tsx
+++ src/BatteryMonitor.tsx
@@ -1,16 +1,40 @@
 import './w3.css';
 
 import React from 'react';
-import { PanelProps, VizOrientation, Field, FieldType, ThresholdsMode, getDisplayProcessor } from '@grafana/data';
-import { BarGauge, BarGaugeDisplayMode, HorizontalGroup } from '@grafana/ui';
+import {
+  PanelProps,
+  VizOrientation,
+  Field,
+  FieldType,
+  ThresholdsMode,
+  getDisplayProcessor,
+  DataFrame,
+  getValueFormat
+} from '@grafana/data';
+import {
+  BarGauge,
+  BarGaugeDisplayMode,
+  HorizontalGroup
+} from '@grafana/ui';
 
-export const BatteryMonitor: React.FC<PanelProps> = ({ options, data, width, height }) => {
+export const BatteryMonitor: React.FC<PanelProps> = (props) => {
   const
+    data = props.data,
     dataA = data.series.find(_ => _.refId === 'A'),
     dataB = data.series.find(_ => _.refId === 'B'),
     dataC = data.series.find(_ => _.refId === 'C'),
     dataD = data.series.find(_ => _.refId === 'D');
-  console.log(dataA, 'test data', dataD?.fields[1].values.get(0));
+
+  const getLastValue = (data: DataFrame | undefined) => {
+    if (data) {
+      const
+        unit = data.fields[1].config.unit,
+        value = data.fields[1].values.get(0);
+      return getValueFormat(unit)(value)
+    } else {
+      return;
+    }
+  }
 
   const field: Partial<Field> = {
     type: FieldType.number,
@@ -33,7 +57,7 @@ export const BatteryMonitor: React.FC<PanelProps> = ({ options, data, width, hei
       <div className="w3-display-middle" style={{ width: '100%' }}>
         <HorizontalGroup justify="center">
           <div className="w3-margin-right">
-            <span className="w3-xxxlarge">{dataA?.fields[1].values.get(0) || 'No Data'}</span><span className="w3-xxlarge">{dataA?.fields[1].values.get(0) ? '%' : ''}</span>
+            <span className="w3-xxxlarge">{getLastValue(dataA)?.text || 'No Data'}</span><span className="w3-xxlarge">{getLastValue(dataA)?.suffix}</span>
           </div>
           <BarGauge
             orientation={VizOrientation.Horizontal}
@@ -52,13 +76,13 @@ export const BatteryMonitor: React.FC<PanelProps> = ({ options, data, width, hei
         </HorizontalGroup>
         <div className="w3-section w3-row">
           <div className="w3-third w3-center">
-            <span className="w3-xlarge">{dataB?.fields[1].values.get(0) || 'No Data'}</span><span className="w3-large">{dataB?.fields[1].values.get(0) ? '%' : ''}</span>
+            <span className="w3-xlarge">{getLastValue(dataB)?.text || 'No Data'}</span><span className="w3-large">{getLastValue(dataB)?.suffix}</span>
           </div>
           <div className="w3-third w3-center">
-            <span className="w3-xlarge">{dataC?.fields[1].values.get(0) || 'No Data'}</span><span className="w3-large">{dataC?.fields[1].values.get(0) ? '%' : ''}</span>
+            <span className="w3-xlarge">{getLastValue(dataC)?.text || 'No Data'}</span><span className="w3-large">{getLastValue(dataC)?.suffix}</span>
           </div>
           <div className="w3-third w3-center">
-            <span className="w3-xlarge">{dataD?.fields[1].values.get(0) || 'No Data'}</span><span className="w3-large">{dataD?.fields[1].values.get(0) ? '%' : ''}</span>
+            <span className="w3-xlarge">{getLastValue(dataD)?.text || 'No Data'}</span><span className="w3-large">{getLastValue(dataD)?.suffix}</span>
           </div>
         </div>
         <div></div>

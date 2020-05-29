2d1e00323992f301a2883225b39542de3944b949
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-05-29 13:36:59 +0700

    update SiteInformation: update another data

--- src/SiteInformation.tsx
+++ src/SiteInformation.tsx
@@ -5,37 +5,58 @@ import {
     PanelProps,
     PanelData
 } from '@grafana/data';
+import { Badge } from '@grafana/ui';
 
 export const SiteInformation: React.FC<PanelProps> = (props) => {
     const data: PanelData = props.data,
-        latitude = data.series[0].fields.find(_ => _.name === 'latitude')?.values.get(0),
-        longitude = data.series[0].fields.find(_ => _.name === 'longitude')?.values.get(0);
+        dataGetter = (dataName: string) => data.series[0].fields.find(_ => _.name === dataName)?.values.get(0),
+        latitude = dataGetter('latitude'),
+        longitude = dataGetter('longitude'),
+        name = dataGetter('name'),
+        region = dataGetter('region'),
+        lastConnected = new Intl.DateTimeFormat("en-GB", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+            hour: 'numeric',
+            minute: 'numeric',
+        }).format(new Date(dataGetter('last_connected')));
 
     const [state, setState] = useState<OpenWeatherData>();
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,daily&units=metric&appid=1f7fd020655b6b19810fbe05adc5b784`)
-        .then(_ => _.json())
-        .then((res: OpenWeatherData) => setState(res));
+    if (typeof state === 'undefined') {
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,daily&units=metric&appid=1f7fd020655b6b19810fbe05adc5b784`)
+            .then(_ => _.json())
+            .then((res: OpenWeatherData) => setState(res));
+    }
 
     return (
-        <div className="w3-row">
-            <div className="w3-half w3-container">
-                <h2>Site Cibolerang</h2>
-            </div>
-            <div className="w3-half">
+        <div className="w3-display-container" style={{ width: '100%', height: '100%' }}>
+            <div className="w3-display-middle" style={{ width: '100%' }}>
                 <div className="w3-row">
-                    <div className="w3-col w3-margin-right w3-center" style={{ width: 150 }}>
-                        <img style={{ position: "relative", marginTop: -20 }} src={state ? `http://openweathermap.org/img/wn/${state.current.weather[0].icon}@2x.png` : ''} />
-                        <h4 style={{ margin: 0 }}>{state?.current.weather[0].main}</h4>
-                        <small>{state?.current.weather[0].description}</small>
+                    <div className="w3-half w3-container w3-center">
+                        <div className="w3-section">
+                            <h3 style={{ margin: 0 }}>{name}</h3>
+                            <span>{region}</span>
+                        </div>
+                        <Badge text={`Last Update: ${lastConnected}`} icon="clock-nine" color="blue"></Badge>
                     </div>
-                    <div className="w3-rest">
-                        <h6 style={{ marginBottom: 0 }}>Temperature</h6>
-                        <div className="w3-margin-left w3-large">{state?.current.temp}<span className="w3-medium"> °C</span></div>
-                        <h6 style={{ marginBottom: 0 }}>Humidity</h6>
-                        <div className="w3-margin-left w3-large">{state?.current.humidity}<span className="w3-medium"> %</span></div>
-                        <h6 style={{ marginBottom: 0 }}>Wind</h6>
-                        <div className="w3-margin-left w3-large">{state?.current.wind_speed} <span className="w3-medium">m/s</span> | {state?.current.wind_deg}<span className="w3-medium"> deg</span></div>
-                    </div>
+                    {state ? <div className="w3-half">
+                        <div className="w3-row">
+                            <div className="w3-col w3-margin-right w3-center" style={{ width: 150 }}>
+                                <img style={{ position: "relative", marginTop: -20 }} src={state ? `http://openweathermap.org/img/wn/${state.current.weather[0].icon}@2x.png` : ''} />
+                                <h4 style={{ margin: 0 }}>{state?.current.weather[0].main}</h4>
+                                <small>{state?.current.weather[0].description}</small>
+                            </div>
+                            <div className="w3-rest">
+                                <h6 style={{ marginBottom: 0 }}>Temperature</h6>
+                                <div className="w3-margin-left w3-large">{state?.current.temp}<span className="w3-medium"> °C</span></div>
+                                <h6 style={{ marginBottom: 0 }}>Humidity</h6>
+                                <div className="w3-margin-left w3-large">{state?.current.humidity}<span className="w3-medium"> %</span></div>
+                                <h6 style={{ marginBottom: 0 }}>Wind</h6>
+                                <div className="w3-margin-left w3-large">{state?.current.wind_speed} <span className="w3-medium">m/s</span> | {state?.current.wind_deg}<span className="w3-medium"> deg</span></div>
+                            </div>
+                        </div>
+                    </div> : null}
                 </div>
             </div>
         </div>

8ef89ddf209c352be639f32e3004ee0eca30bbdf
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-06-05 14:02:42 +0700

    update SiteInformation: fixing display middle with table

---
+++ src/module.ts
@@ -0,0 +1,6 @@
+import { PanelPlugin } from '@grafana/data';
+import { SimpleOptions } from './types';
+import { SiteInformation } from './SiteInformation';
+
+export const plugin = new PanelPlugin<SimpleOptions>(SiteInformation)
+  .useFieldConfig();

84285ee58b53f60c7dfff75f72454b6cd6a0d92d
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2020-06-05 15:13:58 +0700

    update release

--- release/three-phase-monitor-dev/ThreePhaseMonitor.tsx
+++ release/three-phase-monitor-dev/ThreePhaseMonitor.tsx
@@ -8,7 +8,6 @@ import {
     DataFrame,
     getValueFormat
 } from '@grafana/data';
-import { Icon } from '@grafana/ui';
 
 export const ThreePhaseMonitor: React.FC<PanelProps> = (props) => {
     const data: PanelData = props.data,
@@ -29,66 +28,32 @@ export const ThreePhaseMonitor: React.FC<PanelProps> = (props) => {
             return;
         }
     }
-    const minMaxRender = (data: DataFrame | undefined) => {
-        const minMax = (data: DataFrame) => {
-            const _data = data.fields[1].values.toArray();
-            return {
-                min: Math.min.apply(Math, _data.map(_ => _ === null ? Infinity : _)),
-                max: Math.max.apply(Math, _data.map(_ => _ === null ? -Infinity : _))
-            }
-        }
-
-        return data ?
-            <>
-                <small className="tr-min-val"><Icon name="arrow-down" size="sm" />{minMax(data).min}{getLastValue(data)?.suffix}</small>
-                <small className="tr-max-val"><Icon name="arrow-up" size="sm" />{minMax(data).max}{getLastValue(data)?.suffix}</small>
-            </> :
-            null;
-    }
 
     return (
         <div className="w3-display-container" style={{ width: '100%', height: '100%' }}>
             <div className="w3-display-middle" style={{ width: '100%' }}>
-                <div className="w3-row w3-section">
+                <div className="w3-center tr-big-value">
+                    <h1>25.421 <span> kWh</span></h1>
+                </div>
+                <div className="w3-row tr-big-value tr-middle-value">
                     <div className="w3-third w3-center">
-                        <h5 className="w3-pink">Phase R</h5>
-                        <div className="tr-big-value">
-                            <div className="w3-margin-bottom">
-                                <h1>{getLastValue(voltR)?.text}<span>{getLastValue(voltR)?.suffix}</span></h1>
-                                {minMaxRender(voltR)}
-                            </div>
-                            <div className="w3-margin-bottom">
-                                <h1>{getLastValue(currR)?.text}<span>{getLastValue(currR)?.suffix}</span></h1>
-                                {minMaxRender(currR)}
-                            </div>
-                        </div>
+                        <h5>R</h5>
+                        <h1>{getLastValue(currR)?.text}<span>{getLastValue(currR)?.suffix}</span></h1>
+                        <h3>{getLastValue(voltR)?.text}<span>{getLastValue(voltR)?.suffix}</span></h3>
                     </div>
                     <div className="w3-third w3-center">
-                        <h5 className="w3-orange w3-text-white">Phase S</h5>
-                        <div className="tr-big-value">
-                            <div className="w3-margin-bottom">
-                                <h1>{getLastValue(voltS)?.text}<span>{getLastValue(voltS)?.suffix}</span></h1>
-                                {minMaxRender(voltS)}
-                            </div>
-                            <div className="w3-margin-bottom">
-                                <h1>{getLastValue(currS)?.text}<span>{getLastValue(currS)?.suffix}</span></h1>
-                                {minMaxRender(currS)}
-                            </div>
-                        </div>
+                        <h5>S</h5>
+                        <h1>{getLastValue(currS)?.text}<span>{getLastValue(currS)?.suffix}</span></h1>
+                        <h3>{getLastValue(voltS)?.text}<span>{getLastValue(voltS)?.suffix}</span></h3>
                     </div>
                     <div className="w3-third w3-center">
-                        <h5 className="w3-blue">Phase T</h5>
-                        <div className="tr-big-value">
-                            <div className="w3-margin-bottom">
-                                <h1>{getLastValue(voltT)?.text}<span>{getLastValue(voltT)?.suffix}</span></h1>
-                                {minMaxRender(voltT)}
-                            </div>
-                            <div className="w3-margin-bottom">
-                                <h1>{getLastValue(currT)?.text}<span>{getLastValue(currT)?.suffix}</span></h1>
-                                {minMaxRender(currT)}
-                            </div>
-                        </div>
+                        <h5>T</h5>
+                        <h1>{getLastValue(currT)?.text}<span>{getLastValue(currT)?.suffix}</span></h1>
+                        <h3>{getLastValue(voltT)?.text}<span>{getLastValue(voltT)?.suffix}</span></h3>
                     </div>
+                </div>
+                <div className="w3-center tr-middle-value">
+                    <h3>60 <span> Hz</span></h3>
                 </div>
             </div>
         </div>

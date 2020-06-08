50d569c6293aa2272c4d63df892c3c17cc127231
dianramdhani <dian.ramdhani@gmail.com>
2020-06-08 14:50:27 +0700

    update release

--- src/module.ts
+++
@@ -1,5 +0,0 @@
-import { PanelPlugin } from '@grafana/data';
-import { SimpleOptions } from './types';
-import { AlarmSummary } from './AlarmSummary';
-
-export const plugin = new PanelPlugin<SimpleOptions>(AlarmSummary).useFieldConfig();

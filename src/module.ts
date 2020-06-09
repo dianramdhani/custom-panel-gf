ae233c62401be84e7088e00a1cbf41d882fe44d6
dianramdhani <dian.ramdhani@gmail.com>
2020-06-09 14:49:14 +0700

    update ThreePhaseMonitor: add threshold

---
+++ src/module.ts
@@ -0,0 +1,5 @@
+import { PanelPlugin } from '@grafana/data';
+import { SimpleOptions } from './types';
+import { ThreePhaseMonitor } from './ThreePhaseMonitor';
+
+export const plugin = new PanelPlugin<SimpleOptions>(ThreePhaseMonitor).useFieldConfig();

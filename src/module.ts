4169ec54df57e1714293d0461ad1ac8efdcd6828
dianramdhani <dian.ramdhani@gmail.com>
2020-06-10 14:04:02 +0700

    update AlarmSummary: update responsive

---
+++ src/module.ts
@@ -0,0 +1,5 @@
+import { PanelPlugin } from '@grafana/data';
+import { SimpleOptions } from './types';
+import { AlarmSummary } from './AlarmSummary';
+
+export const plugin = new PanelPlugin<SimpleOptions>(AlarmSummary).useFieldConfig();

775fc8868c1368d3451c722f1858ec0ab9b51929
dianramdhani <dian.ramdhani@gmail.com>
2020-06-10 09:44:19 +0700

    update release

--- src/module.ts
+++
@@ -1,5 +0,0 @@
-import { PanelPlugin } from '@grafana/data';
-import { SimpleOptions } from './types';
-import { DCRectifierMonitor } from './DCRectifierMonitor';
-
-export const plugin = new PanelPlugin<SimpleOptions>(DCRectifierMonitor).useFieldConfig();

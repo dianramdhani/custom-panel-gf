34da67c4d379e7097ae0f24ed4e92026aaf7bcd0
dianramdhani <dian.ramdhani@gmail.com>
2020-06-19 16:04:52 +0700

    update BatteryMonitor: finishing view

--- src/BatteryMonitor.tsx
+++ src/BatteryMonitor.tsx
@@ -5,11 +5,16 @@ import React, { PureComponent } from 'react';
 import { PanelProps } from '@grafana/data';
 import { BatteryOptions } from './types';
 import { css } from 'emotion';
+import { Data, lastValueToData, getColor } from './helper';
 
 interface Props extends PanelProps<BatteryOptions> {}
 
 export class BatteryMonitor extends PureComponent<Props> {
   readonly state: { zoom: number };
+  capacity: Data;
+  voltage: Data;
+  resistance: Data;
+  temperature: Data;
 
   constructor(props: Props) {
     super(props);
@@ -17,6 +22,8 @@ export class BatteryMonitor extends PureComponent<Props> {
     this.state = {
       zoom: 1,
     };
+
+    this.capacity = this.voltage = this.resistance = this.temperature = { number: 0, unit: '', color: 'green' };
   }
 
   render() {
@@ -42,10 +49,38 @@ export class BatteryMonitor extends PureComponent<Props> {
   private templates(mode: 'horizontal' | 'vertical'): JSX.Element {
     const { zoom } = this.state;
 
+    const { dataMode } = this.props.options;
+    switch (dataMode) {
+      case 'dummy':
+        const { dummyCapacity, dummyTemperature, dummyResistance, dummyVoltage } = this.props.options;
+        const setValue = (number: number, unit: string): Data => ({
+          number,
+          unit,
+          color: getColor(number, this.props.fieldConfig.defaults.thresholds?.steps),
+        });
+        this.capacity = setValue(dummyCapacity, ' %');
+        this.resistance = setValue(dummyResistance, ' Ohm');
+        this.voltage = setValue(dummyVoltage, ' V');
+        this.temperature = setValue(dummyTemperature, ' C');
+        break;
+      case 'real':
+        const data = this.props.data,
+          cell1 = data.series.find(_ => _.name === 'cell1'),
+          temp1 = data.series.find(_ => _.name === 'temp1'),
+          rest1 = data.series.find(_ => _.name === 'rest1'),
+          soc1 = data.series.find(_ => _.name === 'soc1');
+
+        this.voltage = cell1 ? lastValueToData(cell1) : { number: null, unit: 'No Data', color: 'gray' };
+        this.temperature = temp1 ? lastValueToData(temp1) : { number: null, unit: 'No Data', color: 'gray' };
+        this.resistance = rest1 ? lastValueToData(rest1) : { number: null, unit: 'No Data', color: 'gray' };
+        this.capacity = soc1 ? lastValueToData(soc1) : { number: null, unit: 'No Data', color: 'gray' };
+        break;
+    }
+
     switch (mode) {
       case 'horizontal':
         // reference: https://benohead.com/blog/2014/10/04/html5-displaying-battery-level/
-        const color = 'gray',
+        const color = this.capacity.color,
           battery = css`
             border: 2px solid ${color};
             border-radius: 5px;
@@ -87,21 +122,36 @@ export class BatteryMonitor extends PureComponent<Props> {
         return (
           <div className="w3-display-container tr-full" ref={this.setZoom.bind(this)}>
             <div className="w3-display-middle tr-wd-100" style={{ zoom }}>
-              <div className={`w3-section ${container}`}>
+              <div className={`w3-section ${container} tr-big-value`}>
                 <div className={battery}>
-                  <div className={level} style={{ width: '80%' }}></div>
+                  {this.capacity.number !== null ? (
+                    <div
+                      className={level}
+                      style={{
+                        width: `${this.capacity.number > 100 ? 100 : this.capacity.number}%`,
+                      }}
+                    ></div>
+                  ) : (
+                    undefined
+                  )}
                 </div>
-                <span className="w3-xxlarge">20%</span>
+                <h1 style={{ color: this.capacity.color }}>
+                  {this.capacity.number}
+                  <span>{this.capacity.unit}</span>
+                </h1>
               </div>
               <div className={`w3-section ${container} tr-middle-value`}>
-                <h3 className={smallValue}>
-                  12<span> V</span>
+                <h3 className={smallValue} style={{ color: this.voltage.color }}>
+                  {this.voltage.number}
+                  <span>{this.voltage.unit}</span>
                 </h3>
-                <h3 className={smallValue}>
-                  12<span> V</span>
+                <h3 className={smallValue} style={{ color: this.resistance.color }}>
+                  {this.resistance.number}
+                  <span>{this.resistance.unit}</span>
                 </h3>
-                <h3 className={smallValue}>
-                  12<span> V</span>
+                <h3 className={smallValue} style={{ color: this.temperature.color }}>
+                  {this.temperature.number}
+                  <span>{this.temperature.unit}</span>
                 </h3>
               </div>
             </div>
